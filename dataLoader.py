ec4cf07f469649265db1e7e00c5959e6957aed70
emmadev <tripwithsoo@gmail.com>
2019-07-04 23:47:50 -0700

    add code

--- dataLoader.py
+++ dataLoader.py
@@ -1,27 +1,48 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-
+from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
+import logging
+import json
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import InitializeModels
+import trainModel
+import dataLoader
+from torchvision import models
 class DataLoader:
 
     classes_mnist = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
     classes_CIFAR = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    def __init__(self, valid_size =.2, batch_size = 4, imageType = 'CIFAR'):
+    def __init__(self, valid_size =.2, batch_size = 4, imageSize = 0, imageType = 'CIFAR'):
         super(DataLoader, self).__init__()
 
         self.valid_size = valid_size
         self.batch_size = batch_size
-
-        trainset, testset = setdata()
-        train_sampler,valid_sampler = splitDataset()
-        trainloader, validloader, testloader = loadDataset(train_sampler, valid_sampler)
+        self.imageSize = imageSize
+        self.imageType = imageType
 
     def setdata(self):
 
-        transform = transforms.Compose(
-             [transforms.ToTensor(),
-             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        if self.imageSize == 0:
+            transform = transforms.Compose(
+                 [
+                 transforms.ToTensor(),
+                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        else:
+            transform = transforms.Compose(
+                [
+                    transforms.RandomResizedCrop(imageSize),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                ])
 
         trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                                 download=True, transform=transform)
@@ -29,11 +50,13 @@ class DataLoader:
         testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                                download=True, transform=transform)
 
-    def splitDataset(self, dataset, valid_size):
+        return trainset, testset
+
+    def splitDataset(self, dataset):
 
         lenOfdata = len(dataset)
         indices = list(range(lenOfdata))
-        split = int(np.floor(valid_size * lenOfdata))
+        split = int(np.floor(self.valid_size * lenOfdata))
         np.random.shuffle(indices)
 
         train_idx, valid_idx = indices[split:], indices[:split]
@@ -43,17 +66,18 @@ class DataLoader:
 
         return train_sampler,valid_sampler
 
-    def loadDataset(self, batch_size, train_sampler, valid_sampler):
+    def loadDataset(self, train_sampler, valid_sampler, trainset, testset):
         trainloader = torch.utils.data.DataLoader(
-            trainset, batch_size=batch_size,
+            trainset, batch_size=self.batch_size,
             sampler=train_sampler,
             num_workers=2)
 
         validloader = torch.utils.data.DataLoader(
-            trainset, batch_size=batch_size,
+            trainset, batch_size=self.batch_size,
             sampler=valid_sampler,
             num_workers=2)
 
-        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=self.batch_size, shuffle=False, num_workers=2)
 
         return trainloader, validloader, testloader
+
---
+++ trainModel.py
@@ -0,0 +1,147 @@
+from __future__ import print_function
+from __future__ import division
+import torch
+
+import copy
+import time
+
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False):
+
+    since = time.time()
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    train_losses = list(0. for i in range(num_epochs))
+    test_losses = list(0. for i in range(num_epochs))
+
+    train_accuracies = list(0. for i in range(num_epochs))
+    test_accuracies = list(0. for i in range(num_epochs))
+
+    label_acc_per_epoch = [[0] * num_epochs for i in range(10)]
+    label_val_per_epoch = [[0] * num_epochs for i in range(10)]
+
+    mini_batch = 500
+
+    batch_loss = 0.0
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        epoch_loss = 0.0
+
+        class_correct = list(0. for i in range(10))
+        class_total = list(0. for i in range(10))
+
+        class_correct_train = list(0. for i in range(10))
+        class_total_train = list(0. for i in range(10))
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                correct_train_iter, total_train_iter = 0.0, 0.0
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4 * loss2
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    batch_loss += loss.item()
+
+                    _, preds = torch.max(outputs, 1)
+                    c = (preds == labels).squeeze()
+                    total_train_iter += labels.size(0)  ##32
+                    correct_train_iter += preds.eq(labels).sum().item()  ## 0~32
+
+                    for i in range(len(labels)):
+                        label = labels[i]
+                        class_correct[label] += c[i].item()
+                        class_total[label] += 1
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+
+                    if phase == 'train':
+                        if i % mini_batch == (mini_batch - 1):  # print every 2000 mini-batches
+                            train_acc_iter = correct_train_iter / total_train_iter
+                            train_loss_iter = batch_loss / mini_batch
+
+                            print('\n[%d, %5d] loss: %.3f accuracy: %.3f' % (
+                            epoch + 1, i + 1, train_loss_iter, train_acc_iter))
+
+                            batch_loss = 0.0
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            if phase == 'train':
+                for i in range(10):
+                    acc = class_correct[i] / class_total[i]
+                    label_acc_per_epoch[i][epoch] = acc
+
+                train_losses[epoch] = epoch_loss
+                train_accuracies[epoch] = epoch_acc
+            else:
+                for i in range(10):
+                    acc = class_correct[i] / class_total[i]
+                    label_val_per_epoch[i][epoch] = acc
+
+                test_losses[epoch] = epoch_loss
+                test_accuracies[epoch] = epoch_acc
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
